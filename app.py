89c3a3cf5556c3dfb74d841129e6bdc55b08271e
Siddharth-1703 <khuranasiddharth35@gmail.com>
2026-08-05 10:56:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -83,11 +83,8 @@ st.sidebar.success("API Loaded Successfully")
 # ==============================
 
 model = ChatGoogleGenerativeAI(
-
-    model="gemini-2.5-flash",
-
+    model="gemini-2.0-flash",
     google_api_key=GOOGLE_API_KEY
-
 )
 
 # ==============================
