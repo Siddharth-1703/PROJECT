b4b3a35dbda8c97c64f5b3516abca4176853599f
Siddharth-1703 <khuranasiddharth35@gmail.com>
2026-08-05 18:55:01 +0530

    Update model version from gemini-2.5-flash to gemini-3.5-flash

--- app.py
+++ app.py
@@ -46,7 +46,7 @@ st.sidebar.success("API Loaded Successfully")
 # GEMINI MODEL
 # ==============================
 model = ChatGoogleGenerativeAI(
-    model="gemini-2.5-flash",
+    model="gemini-3.5-flash",
     google_api_key=GOOGLE_API_KEY
 )
 # ==============================
