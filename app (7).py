f6dfe4f9f14e02226a8834ce341499d95935de43
Siddharth-1703 <khuranasiddharth35@gmail.com>
2026-08-05 12:38:51 +0530

    Update app (7).py

--- app (7).py
+++ app (7).py
@@ -70,7 +70,7 @@ st.sidebar.success("API Loaded Successfully")
 # (e.g. "gemini-2.0-flash" or "gemini-1.5-flash"). Change if needed.
 
 model = ChatGoogleGenerativeAI(
-    model="gemini-2.0-flash",
+    model="gemini-3.5-flash",
     google_api_key=GOOGLE_API_KEY
 )
 
